c40d8cea5b45d82d076f4043b4f11271a3499f71
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-07-26 08:47:40 +0500

    ReaderWriterLockSlim

--- src/Cleverence.Library/StaticServers/StaticServer.cs
+++ src/Cleverence.Library/StaticServers/StaticServer.cs
@@ -3,14 +3,37 @@ namespace Cleverence.Library.StaticServers;
 public static class StaticServer
 {
     private static int _count;
+    private static readonly ReaderWriterLockSlim _rwLock;
+
+    static StaticServer()
+    {
+        _count = 0;
+        _rwLock = new ReaderWriterLockSlim();
+    }
     
     public static int GetCount()
     {
-        return _count;
+        _rwLock.EnterReadLock();
+        try
+        {
+            return _count;
+        }
+        finally
+        {
+            _rwLock.ExitReadLock();
+        }
     }
 
     public static void AddToCount(int value)
     {
-        _count = _count + value;
+        _rwLock.EnterWriteLock();
+        try
+        {
+            _count = _count + value;
+        }
+        finally
+        {
+            _rwLock.ExitWriteLock();
+        }
     }
 }
